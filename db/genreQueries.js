2ba06ffc85bcf19c9912b60b7525169358862861
Agoth Arop <agoth.a3@gmail.com>
2026-04-28 11:30:37 -0600

    genreQueries implemented

--- db/genreQueries.js
+++ db/genreQueries.js
@@ -3,41 +3,60 @@ const pool = require("./pool");
 // GET /genres
 const getAllGenres = async () => {
 	const { rows } = await pool.query(`
-        
+        SELECT * FROM genres;
         `);
 	return rows;
 };
 
 // GET /genres/:id
-const getGenreById = async ({ genreId }) => {
-	const { rows } = await pool.query(`
-        
-        `);
+const getGenreById = async (genreId) => {
+	const { rows } = await pool.query(
+		`
+        SELECT * FROM genres
+        WHERE id = $1
+        `,
+		[genreId],
+	);
 
 	return rows[0];
 };
 
 // POST /genres/
-const insertGenre = async ({ genreName }) => {
-	const { rows } = await pool.query(`
-        
-        `);
+const insertGenre = async (genreName) => {
+	const { rows } = await pool.query(
+		`
+        INSERT INTO genres (name) 
+        VALUES $1 RETURNING *;
+        `,
+		[genreName],
+	);
 	return rows[0];
 };
 
 // PUT /genres/:id
 const updateGenre = async (genreId, { genreName }) => {
-	const { rows } = await pool.query(`
-            
-        `);
+	const { rows } = await pool.query(
+		`
+        UPDATE genres
+        SET name = $1
+        WHERE id = $2
+        RETURNING *;        
+        `,
+		[genreName, genreId],
+	);
 	return rows[0];
 };
 
 // DELETE /genres/:id
-const deleteGenre = async ({ genreId }) => {
-	const { rows } = await pool.query(`
-        
-        `);
+const deleteGenre = async (genreId) => {
+	const { rows } = await pool.query(
+		`
+        DELETE FROM genres
+        WHERE id = $1
+        RETURNING *;
+        `,
+		[genreId],
+	);
 	return rows[0];
 };
 
